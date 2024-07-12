495420221a1266b7397edae72a714dfd507ff809
argarifullin <garifullin.alexandr@gmail.com>
2024-07-12 14:11:00 +0300

    SCOM-1120: fixed issue with csp helper

--- Helper/CspHelper.php
+++ Helper/CspHelper.php
@@ -4,23 +4,13 @@ namespace Scommerce\CspHelper\Helper;
 
 use Magento\Framework\App\Helper\Context;
 
-if (class_exists('Magento\Csp\Helper\CspNonceProvider')) {
-    class CspHelperMiddle extends \Magento\Csp\Helper\CspNonceProvider { }
-} else {
-    class CspHelperMiddle {
-        public function generateNonce() {
-            return false;
-        }
-    }
-}
-
 class CspHelper extends \Magento\Framework\App\Helper\AbstractHelper
 {
     protected $cspHelperMiddle;
 
     public function __construct(
         Context $context,
-        CspHelperMiddle $cspHelperMiddle
+        \Scommerce\CspHelper\Helper\CspHelperMiddle $cspHelperMiddle
     ) {
         parent::__construct($context);
         $this->cspHelperMiddle = $cspHelperMiddle;
